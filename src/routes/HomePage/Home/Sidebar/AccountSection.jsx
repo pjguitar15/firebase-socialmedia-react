acccb4a9c9e7f1851d23fef8922608be3df622e4
Phil <pjguitar15@gmail.com>
2021-04-05 11:47:03 +0800

    sidebar Account User current logged in

--- src/routes/HomePage/Home/Sidebar/AccountSection.jsx
+++ src/routes/HomePage/Home/Sidebar/AccountSection.jsx
@@ -5,15 +5,20 @@ import {
   Container,
   CircleDiv,
 } from './Styles/Main.style.jsx'
+//  import auth
+import { useAuth } from '../../../../Context/AuthProvider.jsx'
 const AccountSection = () => {
+  const { currentUser } = useAuth()
   return (
     <Container>
       <SmallBoldText>Account</SmallBoldText>
       <div className='d-flex justify-content-start my-4'>
         <CircleDiv className='mx-2'></CircleDiv>
         <div className='mx-2'>
-          <SmallBoldText style={{ fontSize: '14.5px' }}>Philcob</SmallBoldText>
-          <SmallMutedText>@pjguitar15</SmallMutedText>
+          <SmallBoldText style={{ fontSize: '14.5px' }}>
+            {currentUser.email.slice(0, currentUser.email.indexOf('@'))}
+          </SmallBoldText>
+          <SmallMutedText>{currentUser.email}</SmallMutedText>
         </div>
       </div>
     </Container>
